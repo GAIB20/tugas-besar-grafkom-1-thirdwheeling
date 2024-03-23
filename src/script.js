7e99b6177afca3dcc7e360381ba18fafd48a2e7c
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2024-03-23 21:38:51 +0700

    fix: create line
    Co-authored-by: Eunice Sarah Siregar <eunicesarah@users.noreply.github.com>

--- src/script.js
+++ src/script.js
@@ -1,69 +1,174 @@
-"use strict"
-function createShader(gl, type, source){
-    let shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-    if (success) {
-      return shader;
-    }
-    console.log(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
+/* eslint no-console:0 consistent-return:0 */
+"use strict";
+
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
 }
 
 function createProgram(gl, vertexShader, fragmentShader) {
-    let program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    let success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-      return program;
-    }
- 
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
 }
 
-let canvas = document.querySelector("#canvas");
-var gl = canvas.getContext("webgl");
-let vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
-let fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
-let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-let program = createProgram(gl, vertexShader, fragmentShader);
+function createLine(gl, positions){
+    // console.log(x,y)
 
+    console.log(positions);
+    // var positions = [
+    //     0, 0,
+    //     1, 0.5,
+    // ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    
+    // code above this line is initialization code.
+    // code below this line is rendering code.
+    
+    
+      // Tell WebGL how to convert from clip space to pixels
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    
+      // Clear the canvas
+      gl.clearColor(0, 0, 0, 0);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+    
+      // Tell it to use our program (pair of shaders)
+      gl.useProgram(program);
+    
+      // Turn on the attribute
+      gl.enableVertexAttribArray(positionAttributeLocation);
+    
+      // Bind the position buffer.
+      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    
+      // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+      var size = 2;          // 2 components per iteration
+      var type = gl.FLOAT;   // the data is 32bit floats
+      var normalize = false; // don't normalize the data
+      var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+      var offset = 0;        // start at the beginning of the buffer
+      gl.vertexAttribPointer(
+          positionAttributeLocation, size, type, normalize, stride, offset);
+    
+      // draw
+      var primitiveType = gl.LINES;
+      var offset = 0;
+      var count = 1;
+      gl.drawArrays(primitiveType, offset, count);
+}
 
-let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-let positionBuffer = gl.createBuffer();
+function main() {
+  // Get A WebGL context
+  var canvas = document.querySelector("#canvas");
+  var gl = canvas.getContext("webgl", {antialias: true});
+  if (!gl) {
+    console.log("hahahah gak punya web gl")
+    return;
+}
+
+// Get the strings for our GLSL shaders
+var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
+
+// create GLSL shaders, upload the GLSL source, compile the shaders
+var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+// Link the two shaders into a program
+var program = createProgram(gl, vertexShader, fragmentShader);
+
+// look up where the vertex data needs to go.
+var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+// Create a buffer and put three 2d clip space points in it
+var positionBuffer = gl.createBuffer();
+
+// Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-let positions = [
-  0, 0,
-  0, 0.5,
-  0.7, 0,
-];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-// gl.drawArrays(gl.TRIANGLES,0,3)
-var positionLoc = gl.getAttribLocation(someShaderProgram, "a_position");
-gl.enableVertexAttribArray(positionLoc);
- 
-var numComponents = 3;  // (x, y, z)
-var type = gl.FLOAT;    // 32bit floating point values
-var normalize = false;  // leave the values as they are
-var offset = 0;         // start at the beginning of the buffer
-var stride = 0;         // how many bytes to move to the next vertex
-                        // 0 = use the correct stride for type and numComponents
- 
-gl.vertexAttribPointer(positionLoc, numComponents, type, normalize, stride, offset);
 
+let x;
+let y;
+let click = [];
 
-if (!gl) {
-  console.log("hahaha")
+canvas.addEventListener("click", function(event){
+    let rect = canvas.getBoundingClientRect();
+    x = event.clientX;
+    y = event.clientY;
+
+    
+    // console.log("clicked at "+x+","+y);
+    click.push({ x, y });
+
+    if (click.length === 2) {
+    //   gl.clear(gl.COLOR_BUFFER_BIT);
+
+      var positions = [
+        click[0].x/1000, click[0].y/1000,
+        click[1].x/1000, click[1].y/1000,
+        // 0, 0,
+        // 0, 0.5,
+      ];
+
+    //   createLine(gl, positions);
+      console.log(positions)
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    
+    // code above this line is initialization code.
+    // code below this line is rendering code.
+    
+    
+      // Tell WebGL how to convert from clip space to pixels
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    
+      // Clear the canvas
+      gl.clearColor(0, 0, 0, 0);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+    
+      // Tell it to use our program (pair of shaders)
+      gl.useProgram(program);
+    
+      // Turn on the attribute
+      gl.enableVertexAttribArray(positionAttributeLocation);
+    
+      // Bind the position buffer.
+      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    
+      // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+      var size = 2;          // 2 components per iteration
+      var type = gl.FLOAT;   // the data is 32bit floats
+      var normalize = false; // don't normalize the data
+      var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+      var offset = 0;        // start at the beginning of the buffer
+      gl.vertexAttribPointer(
+          positionAttributeLocation, size, type, normalize, stride, offset);
+    
+      // draw
+      var primitiveType = gl.LINES;
+      var offset = 0;
+      var count = 2;
+      gl.drawArrays(primitiveType, offset, count);
+
+      click = [];
+    }
+})
+
 }
 
-// gl.enableVertexAttribArray(positionAttributeLocation);
-// gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-
-// gl.clearColor(1,1,1,1);
-// gl.clear(gl.COLOR_BUFFER_BIT);
-// gl.drawArrays(gl.LINES, 0, 3); // Draw a line with 2 vertices
+main();
